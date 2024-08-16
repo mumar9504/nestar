53e9d670d1ceea911e3fb88033bac83f7383d814
mumar9504 <minu0709ms@gmail.com>
2024-08-16 20:54:19 +0900

    fix: modify first GraphQL api

--- apps/nestar-api/src/app.resolver.ts
+++ apps/nestar-api/src/app.resolver.ts
@@ -3,7 +3,7 @@ import {Query, Resolver} from '@nestjs/graphql';
 @Resolver()
 export class AppResolver {
   @Query(() => String)
-  public sayhello(): string {
+  public sayHello(): string {
     return 'GraphQL API Server';
   }
 }
