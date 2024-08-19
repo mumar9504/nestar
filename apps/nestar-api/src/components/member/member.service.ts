c09387bb1dc121a4b5c8e9f8ca9ea439083f654c
mumar9504 <minu0709ms@gmail.com>
2024-08-19 23:09:46 +0900

    feat: develop login business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,8 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Member } from '../../libs/dto/member/member';
-import { MemberInput } from '../../libs/dto/member/member.input.dto';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input.dto';
+import { MemberStatus } from '../../libs/enums/member.enum';
+import { Int } from '@nestjs/graphql';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class MemberService {
@@ -14,15 +17,33 @@ export class MemberService {
 		try {
 			const result = await this.memberModel.create(input);
 			// TODO: Authentification via Token
-      return result;
+			return result;
 		} catch (err) {
 			console.log('Error, Service.model:', err);
 			throw new BadRequestException(err);
 		}
 	}
 
-	public async login(): Promise<string> {
-		return 'login executed!';
+	public async login(input: LoginInput): Promise<Member> {
+		const { memberNick, memberPassword } = input;
+		const response: Member = await this.memberModel
+			.findOne({ memberNick: memberNick })
+			.select('+memberPassword')
+			.exec();
+
+    if (!response || response.memberStatus === MemberStatus.DELETE) {
+      throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+    } else if (response.memberStatus === MemberStatus.BLOCK) {
+      throw new InternalServerErrorException(Message.BLOCKED_USER);
+    } 
+
+    // TODO: compare password
+    console.log('response:', response);
+    
+    const isMatch = memberPassword === response.memberPassword;
+    if (!isMatch) {throw new InternalServerErrorException(Message.WRONG_PASSWORD);}
+    
+		return response;
 	}
 
 	public async updateMember(): Promise<string> {
