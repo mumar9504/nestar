da9ea320f73aedc28fe558a1ed8caace7b2c5306
mumar9504 <minu0709ms@gmail.com>
2024-08-25 20:13:09 +0900

    feat: integration of Authentication and Authorization

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -2,6 +2,10 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
 
 @Resolver()
 export class MemberResolver {
@@ -9,21 +13,33 @@ export class MemberResolver {
 
 	@Mutation(() => Member)
 	public async signup(@Args('input') input: MemberInput): Promise<Member> {
-			console.log('Mutation: signup');
-			return this.memberService.signup(input);
+		console.log('Mutation: signup');
+		return this.memberService.signup(input);
 	}
 
 	@Mutation(() => Member)
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
-			console.log('Mutation: login');
-			return this.memberService.login(input);
+		console.log('Mutation: login');
+		return this.memberService.login(input);
 	}
 
 	// Authenticated (USER, AGENT, ADMIN => bu yerda faqatgina LOGIN bo'lgan user bo'lsa yetarli)
+	@UseGuards(AuthGuard)
 	@Mutation(() => String)
-	public async updateMember(): Promise<string> {
+	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
 		console.log('Mutation: updateMember');
+		//console.log(typeof memberId);
+		//console.log(memberId);
 		return this.memberService.updateMember();
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => String)
+	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
+		console.log('Mutation: checkAuth');
+		//console.log(typeof memberId);
+		console.log('memberNick:', memberNick);
+		return `Hello, ${memberNick}!`;
 	}
 
 	@Query(() => String)
@@ -33,7 +49,7 @@ export class MemberResolver {
 	}
 
 	/** ADMIN **/
-	
+
 	// Authorization: ADMIN
 	@Mutation(() => String)
 	public async getAllMembersByAdmin(): Promise<string> {
@@ -46,5 +62,4 @@ export class MemberResolver {
 		console.log('Mutation: updateMemberByAdmin');
 		return this.memberService.updateMemberByAdmin();
 	}
-
 }
