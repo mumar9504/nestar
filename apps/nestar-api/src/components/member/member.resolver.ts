a9aa309b1e7d7871d720c0513ce762cd827eefd8
mumar9504 <minu0709ms@gmail.com>
2024-09-08 19:03:24 +0900

    feat: develop likeTargetMember GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -78,6 +78,17 @@ export class MemberResolver {
 		return await this.memberService.getAgents(memberId, input);
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async likeTargetMember(
+		@Args('memberId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation: likeTargetMember');
+		const likeRefId = shapeIntoMongoObjectId(input);
+		return await this.memberService.likeTargetMember(memberId, likeRefId);
+	}
+
 	/** ADMIN **/
 
 	// Authorization: ADMIN
@@ -98,7 +109,7 @@ export class MemberResolver {
 	}
 
 	/** UPLOADER **/
-	
+
 	@UseGuards(AuthGuard)
 	@Mutation((returns) => String)
 	public async imageUploader(
